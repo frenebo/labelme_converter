e3366ad50ef7c5b28a044325a6850f1948e25f56
Paul Kreymborg <frenebo@gmail.com>
2019-03-27 13:19:18 -0700

    minor change

--- example.py
+++ example.py
@@ -3,10 +3,12 @@ if __name__ == "__main__":
     from labelme_converter import convert_xml_labels_to_csv_lines
 
     example_files = ["example_annotations.xml", "example_no_annotations.xml"]
+
     for file_name in example_files:
         with open(file_name, "r") as example_file:
             converted_lines = convert_xml_labels_to_csv_lines(example_file.read())
-            print(file_name + ":")
-            for line in converted_lines:
-                print("    " + line)
-            # print(converted_lines)
+
+        print(file_name + ":")
+
+        for line in converted_lines:
+            print("    " + line)
